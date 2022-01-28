2411688b6ac8593493c287166800513240928fd4
sahibul_nf <sahibulnuzulfirdaus13@gmail.com>
2022-01-29 04:06:32 +0700

    feat: new server baseurl

--- nuxt.config.js
+++ nuxt.config.js
@@ -76,7 +76,7 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: "http://localhost:8080"
+    baseURL: "https://moyu-api.herokuapp.com"
   },
 
   auth: {
