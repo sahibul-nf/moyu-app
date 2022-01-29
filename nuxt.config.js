2bf94883fac41306ce8b9a9b2d71268ee30a8b64
sahibul_nf <sahibulnuzulfirdaus13@gmail.com>
2022-01-29 20:56:01 +0700

    fix 🐛: upload campaign image

--- nuxt.config.js
+++ nuxt.config.js
@@ -77,6 +77,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     baseURL: "https://moyu-api.herokuapp.com"
+    // baseURL: "http://localhost:8080"
   },
 
   auth: {
